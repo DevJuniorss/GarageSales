243b5f691c8fc3b002bf5910dbb89e6391d12c05
DevJuniorss <devjuniorss@gmail.com>
2025-07-30 05:26:56 -0300

    filtrando item disponiveis no meu estoque

---
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -0,0 +1,35 @@
+INSERT INTO clothes (id, sex, dresstype, image, price, arrival, discount) VALUES
+-- WHEY Chocolate
+-- (100, 'SUPPLEMENT', 'Whey Protein Chocolate - 1kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/choco.png', 'R$ 149,90', 'new', 10),
+(101, 'SUPPLEMENT', 'Whey Protein Chocolate - 2 kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/coch.png', 'R$ 120,00', 'new', 15),
+-- (102, 'SUPPLEMENT', 'Whey Protein Chocolate - 1kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/rotulo.png', 'R$ 149,90', 'new', 12),
+-- (103, 'SUPPLEMENT', 'Whey Protein Chocolate - 1kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/rotulozoom.png', 'R$ 149,90', 'old', 20),
+
+-- WHEY Vanilla
+-- (104, 'SUPPLEMENT', 'Whey Protein Vanilla - 1kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/vanilla.png', 'R$ 144,90', 'new', 18),
+(105, 'SUPPLEMENT', 'Whey Protein Vanilla - 2 kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/vanilla1.png', 'R$ 120,00', 'new', 10),
+
+-- WHEY Cookies
+(106, 'SUPPLEMENT', 'Whey Protein Cookies & Cream - 2 kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/cockie.png', 'R$ 120,90', 'new', 5),
+
+-- WHEY Morango
+-- (107, 'SUPPLEMENT', 'Whey Protein Morango - 1kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/morango.png', 'R$ 149,90', 'new', 10),
+(108, 'SUPPLEMENT', 'Whey Protein Morango - 2 kg', 'kerozennelogo.s3.us-east-1.amazonaws.com/morango1.png', 'R$ 120,00', 'old', 25),
+
+-- Creatina Creapure
+-- (109, 'SUPPLEMENT', 'Creatina Creapure - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/creapure.png', 'R$ 99,90', 'new', 10),
+-- (110, 'SUPPLEMENT', 'Creatina Creapure - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/creapure1.png', 'R$ 99,90', 'new', 12),
+-- (111, 'SUPPLEMENT', 'Creatina Creapure - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/creapure2.png', 'R$ 99,90', 'old', 20),
+-- (112, 'SUPPLEMENT', 'Creatina Creapure - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/creapurefront.png', 'R$ 99,90', 'old', 20),
+-- (113, 'SUPPLEMENT', 'Creatina Creapure - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/creapurerotulo.png', 'R$ 99,90', 'new', 10),
+
+-- Creatina Pura
+-- (114, 'SUPPLEMENT', 'Creatina Monohidratada - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/creatina1.png', 'R$ 89,90', 'new', 18),
+(115, 'SUPPLEMENT', 'Creatina Monohidratada - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/creatinapropa.png', 'R$ 55,00', 'new', 15),
+-- (116, 'SUPPLEMENT', 'Creatina Monohidratada - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/creatinarotulo.png', 'R$ 89,90', 'old', 25),
+
+-- Pré-treino Panic
+-- (117, 'SUPPLEMENT', 'Pré-Treino Panic - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/panic.png', 'R$ 79,90', 'new', 10),
+(118, 'SUPPLEMENT', 'Pré-Treino Panic - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/panic1.png', 'R$ 109,90', 'new', 5),
+-- (119, 'SUPPLEMENT', 'Pré-Treino Panic - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/panic2.png', 'R$ 79,90', 'old', 20),
+-- (120, 'SUPPLEMENT', 'Pré-Treino Panic - 300g', 'kerozennelogo.s3.us-east-1.amazonaws.com/panic3.png', 'R$ 79,90', 'old', 15);
